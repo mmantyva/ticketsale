cb3156b0ae90b35372272e7b62fa3380c1d37f0a
mmantyva <60605429+mmantyva@users.noreply.github.com>
2021-05-05 17:11:23 +0300

    heroku fix 2

--- src/components/TicketSale.js
+++ src/components/TicketSale.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import MaterialTable from 'material-table';
 import {Link} from 'react-router-dom';
-import TableIcons from './TableIcons';
+import TableIcons from './TableIcons.js';
 
 function Events() {
 
